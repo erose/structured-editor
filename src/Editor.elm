acd2b7eca0de4e2f1c2de072a45a958b5b0198f9
Geoffrey Litt <gklitt@gmail.com>
2019-08-14 11:52:51 -0400

    highlight a hardcoded node

--- src/Editor.elm
+++ src/Editor.elm
@@ -7,9 +7,14 @@ import Html.Events
 import Json.Decode
 
 
+-- TODOS
+-- highlighting, show results
+-- user input
+-- subtraction and division
+-- arithmetic -> lisp
+
 
 -- MODEL
-
 
 type Node
     = NumNode Int -- Numeric literal node: e.g. '4'
@@ -25,10 +30,9 @@ type Operator
 
 type alias Model =
     { -- Abstract Syntax Tree
-      ast : Node
+      ast : Node,
+      selectedNode: Maybe Node
     }
-
-
 
 -- MSG
 
@@ -40,13 +44,15 @@ type Msg
 init : flags -> ( Model, Cmd Msg )
 init _ =
     let
+        innerNode = FnNode Multiply [ NumNode 3, NumNode 2 ]
         initialAST =
             FnNode Add
-                [ FnNode Multiply [ NumNode 3, NumNode 2 ]
-                , NumNode 10
+                [
+                    innerNode,
+                    NumNode 10
                 ]
     in
-    ( { ast = initialAST }, Cmd.none )
+    ( { ast = initialAST, selectedNode = Just innerNode }, Cmd.none )
 
 
 main : Program () Model Msg
@@ -67,14 +73,16 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    Html.div [] [ viewNode model.ast ]
+    Html.div [] [ viewNode model.ast model.selectedNode ]
 
 
-viewNode : Node -> Html Msg
-viewNode node =
+viewNode : Node -> Maybe Node -> Html Msg
+viewNode node selectedNode =
     let
         baseStyles =
-            [ style "margin" "5px", style "padding-left" "2px", style "display" "inline-block", style "border-radius" "5px" ]
+            [ style "font-size" "30px", style "margin" "8px", style "padding-left" "5px", style "display" "inline-block", style "border-radius" "5px" ]
+        
+        viewChild childNode = viewNode childNode selectedNode
     in
     case node of
         FnNode operator children ->
@@ -82,18 +90,24 @@ viewNode node =
                 operatorSpan =
                     Html.span [] [ Html.text <| renderOperator operator ]
 
+                blue = "rgba(44, 178, 218, 0.2)"
+                orange = "rgba(255, 165, 0, 0.5)"
+                backgroundColor = case selectedNode of
+                    Just n -> if node == n then orange else blue
+                    Nothing -> blue
+
                 styles =
-                    style "background-color" "rgba(44, 178, 218, 0.2)" :: baseStyles
+                    style "background-color" backgroundColor :: baseStyles
 
                 onClickHandler =
                     -- We are stacking multiple spans with event handlers all on top of each other;
                     -- hence, we need to stop propagation in our event handler. Unfortunately, this
                     -- requries us to use a more complex API than Html.Events.onClick.
-                    Html.Events.stopPropagationOn "click" <| Json.Decode.succeed ( DisplayValue node, True )
+                    Html.Events.stopPropagationOn "mouseover" <| Json.Decode.succeed ( DisplayValue node, True )
             in
             Html.span
                 ([ onClickHandler, class "fn-node node" ] ++ styles)
-                ([ operatorSpan ] ++ List.map viewNode children)
+                ([ operatorSpan ] ++ List.map viewChild children)
 
         NumNode content ->
             Html.span
